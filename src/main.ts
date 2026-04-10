ea9094a0e0ca49aa049e4b0dfa0340568f815c22
변도휘 <dohwe0978@gmail.com>
2026-04-10 11:08:01 +0900

    feat: (#5) 메모 모듈 등록 및 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-﻿import { NestFactory } from '@nestjs/core';
+﻿import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: true, credentials: true });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   const port = Number(process.env.PORT || 3000);
   await app.listen(port);
 }
