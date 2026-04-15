bfcbcd72c6f0bdd51bff4b110a251da0c5d61e5b
dohwi <dohwe0978@gmail.com>
2026-04-15 09:32:27 +0900

    feat: (#7) swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 ﻿import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Spot Log API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   const port = Number(process.env.PORT || 3000);
   await app.listen(port);
 }
