dc6e754acf37808d5d2c6127b6fe7e86cbc2d53b
dohwi <dohwe0978@gmail.com>
2026-04-27 12:17:44 +0900

    feat: 스케줄러 확인

--- src/memo/memos.module.ts
+++ src/memo/memos.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../users/users.module';
-import { MemoController } from './memo.controller';
+import { MemoBookmark } from './entity/memo-bookmark.entity';
 import { Memo } from './entity/memo.entity';
+import { MemoExpiryScheduler } from './memo-expiry.scheduler';
+import { MemoController } from './memo.controller';
 import { MemosService } from './memos.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Memo]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Memo, MemoBookmark]), UsersModule],
   controllers: [MemoController],
-  providers: [MemosService],
+  providers: [MemosService, MemoExpiryScheduler],
   exports: [MemosService]
 })
 export class MemosModule {}
